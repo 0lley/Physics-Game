bfbe23dfa08c5b4a78ba5eb9c73e1d617757cdd2
0lley <39053286+0lley@users.noreply.github.com>
2019-12-09 17:32:02 +1100

    Update Display.js

--- Display.js
+++ Display.js
@@ -1,28 +1,34 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    planet = new Star()
+    planet = new Stellar()
+}
+
+function mouseClicked() {
+    planet = new Stellar()
 }
 
 function draw() {
-    fill(227 ,0, 80);
-    background(21);
-    noStroke()
+    background(11);
     planet.display()
+    console.log(planet.distance())
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight)
 }
 
-class Star {
+class Stellar {
     constructor() {
       this.x = windowWidth / 2;
       this.y = windowHeight / 2;
-      this.d = int(prompt("How big, b?"));
+      this.d = 99;
     }
   
     display() {
       ellipse(this.x, this.y, this.d);
+    }
+    distance() {
+        return sqrt(Math.pow(this.x - mouseX, 2) + Math.pow(this.y - mouseY, 2))
     }
   }
   
